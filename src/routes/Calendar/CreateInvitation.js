73a93e5ef8ccbb942ef9a9174c289a358ab0c811
wangshuaishuai.wss <wangshuai@wangshuaideMacBook-Pro.local>
2018-05-02 16:41:52 +0800

    wss

--- src/routes/Calendar/CreateInvitation.js
+++ src/routes/Calendar/CreateInvitation.js
@@ -212,7 +212,7 @@ export default class Creat extends PureComponent {
                     repeatTypeCode:this.state.chongfu,
                     address:this.state.place,
                     remark:this.state.beizhu,
-                    repeatEndTime:params.endTime
+                    yearId:params.yearId
                   }
               }).then(function(res){
                        console.log(_this.props.yaoyue
